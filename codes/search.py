936ca828bb99766284c8108b95f572316c8e0e57
Amlan <amlanaccount@gmail.com>
2020-04-14 22:35:19 -0400

    Added experiment facility

--- codes/search.py
+++ codes/search.py
@@ -1,6 +1,41 @@
 from utils import *
 import pdb
 
+
+# -----------------------------------------------
+class Parameters:
+
+    # -----------------------------------------------
+    def __init__(self, params):
+
+        self.problem_type = self.getDictVal(params, 'Problem Type')
+        self.exp_id = self.getDictVal(params, 'Experiment ID')
+        self.random_seed = int(self.getDictVal(params, 'Random Number Seed'))
+        self.num_runs = int(self.getDictVal(params, 'Number of Runs'))
+        self.pop_size = int(self.getDictVal(params, 'Population Size'))
+        self.num_gens = int(self.getDictVal(params, 'Generations per Run'))
+        self.sel_type = int(self.getDictVal(params, 'Selection Method (1)'))
+        self.scale_type = int(self.getDictVal(params, 'Fitness Scaling Type (2)'))
+        self.xover_type = int(self.getDictVal(params, 'Crossover Type (3)'))
+        self.xover_rate = float(self.getDictVal(params, 'Crossover Rate (4)'))
+        self.mut_type = int(self.getDictVal(params, 'Mutation Type (5)'))
+        self.mut_rate = float(self.getDictVal(params, 'Mutation Rate (6)'))
+        self.num_genes = int(self.getDictVal(params, 'Number of Genes/Points (7)'))
+        self.gene_size = int(self.getDictVal(params, 'Size of Genes (18)'))
+
+        # Rank selection
+        if self.sel_type == 4:
+            self.sel_type = 1
+            if self.scale_type == 0:
+                self.scale_type = 2
+            elif self.scale_type == 1:
+                self.scale_type = 3
+
+    # -----------------------------------------------
+    def getDictVal(self, d, key):
+        return d[key] if key in d else None
+
+
 # -----------------------------------------------
 class Chromosome:
 
@@ -10,20 +45,20 @@ class Chromosome:
         self.raw_fitness = -1
         self.scl_fitness = -1
         self.pro_fitness = -1
-        
+
         self.chromo = ''
-        for i in range(num_genes):
-            for j in range(gene_size):
+        for i in range(params.num_genes):
+            for j in range(params.gene_size):
                 self.chromo += '0' if random.random() < 0.5 else '1'
 
     # -----------------------------------------------
     def mutate(self):
 
-        assert mut_type == 1, 'Error: Invalid mutation type {}'.format(mut_type)
+        assert params.mut_type == 1, 'Error: Invalid mutation type {}'.format(params.mut_type)
 
         x = ''
-        for i in range(num_genes * gene_size):
-            if random.random() < mut_rate:
+        for i in range(params.num_genes * params.gene_size):
+            if random.random() < params.mut_rate:
                 x += '0' if self.chromo[i] == '1' else '0'
             else:
                 x += self.chromo[i]
@@ -52,48 +87,48 @@ class Chromosome:
         print(self.scl_fitness)
         print(self.pro_fitness)
 
+
 # -----------------------------------------------
-def selectParent():
+def selectParent(members):
+    assert params.sel_type in [1, 2, 3], 'Error: Invalid selection type {}'.format(params.sel_type)
 
-    assert sel_type in [1, 2, 3], 'Error: Invalid selection type {}'.format(sel_type)
-
-    if sel_type == 1:
+    if params.sel_type == 1:
         r_wheel = 0
         r = random.random()
-        for i in range(pop_size):
+        for i in range(params.pop_size):
             r_wheel += members[i].pro_fitness
             if r < r_wheel:
                 return i
         return -1
 
-    elif sel_type == 2:    
+    elif params.sel_type == 2:
         tour_size = 0.5
         tour_prob = 0.5
 
-        tournament = random.sample(range(pop_size), int(tour_size*pop_size))
+        tournament = random.sample(range(params.pop_size), int(params.tour_size * params.pop_size))
         tournament = sorted(tournament, key=lambda i: members[i].pro_fitness, reverse=True)
-        
+
         for i in range(len(tournament)):
             if random.random() < tour_prob:
                 return tournament[i]
         return tournament[-1]
 
-    elif sel_type == 3:
-        return random.randint(0, pop_size-1)
+    elif params.sel_type == 3:
+        return random.randint(0, params.pop_size - 1)
+
 
 # -----------------------------------------------
 def xover(p1, p2):
-
-    assert xover_type in [1, 2, 3], 'Error: Invalid cross over type {}'.format(xover_type)
+    assert params.xover_type in [1, 2, 3], 'Error: Invalid cross over type {}'.format(params.xover_type)
 
     c1 = Chromosome()
     c2 = Chromosome()
 
-    if xover_type == 1:
+    if params.xover_type == 1:
         # -----------------------------------------------
         # Select crossover point
         # -----------------------------------------------
-        xp = random.randint(0, num_genes * gene_size-1)
+        xp = random.randint(0, params.num_genes * params.gene_size - 1)
 
         # -----------------------------------------------
         # Create child chromosome from parental material
@@ -101,12 +136,12 @@ def xover(p1, p2):
         c1.chromo = p1.chromo[:xp] + p2.chromo[xp:]
         c2.chromo = p2.chromo[:xp] + p1.chromo[xp:]
 
-    elif xover_type == 2:
+    elif params.xover_type == 2:
         # -----------------------------------------------
         # Select crossover points
         # -----------------------------------------------
-        xp1 = random.randint(0, num_genes * gene_size-1)
-        xp2 = random.randint(0, num_genes * gene_size-1)
+        xp1 = random.randint(0, params.num_genes * params.gene_size - 1)
+        xp2 = random.randint(0, params.num_genes * params.gene_size - 1)
 
         if xp1 > xp2:
             xp1, xp2 = xp2, xp1
@@ -119,52 +154,55 @@ def xover(p1, p2):
 
     return c1, c2
 
-# -----------------------------------------------
-def scaleFitness():
 
-    assert scale_type in [0, 1, 2, 3], 'Error: Invalid fitness scaling type'
+# -----------------------------------------------
+def scaleFitness(members):
+    assert params.scale_type in [0, 1, 2, 3], 'Error: Invalid fitness scaling type {}'.format(params.scale_type)
 
     sum_sf = 0
-    if scale_type == 0:
-        for i in range(pop_size):
+    if params.scale_type == 0:
+        for i in range(params.pop_size):
             members[i].scl_fitness = members[i].raw_fitness + 0.000001
             sum_sf += members[i].scl_fitness
 
-    elif scale_type == 1:
-        for i in range(pop_size):
-            members[i].scl_fitness = 1/(members[i].raw_fitness + 0.000001)
+    elif params.scale_type == 1:
+        for i in range(params.pop_size):
+            members[i].scl_fitness = 1 / (members[i].raw_fitness + 0.000001)
             sum_sf += members[i].scl_fitness
 
-    elif scale_type == 2:
-        member_indices = sorted(range(pop_size), key=lambda i: members[i].raw_fitness)
-        for i in range(pop_size):
+    elif params.scale_type == 2:
+        member_indices = sorted(range(params.pop_size), key=lambda i: members[i].raw_fitness)
+        for i in range(params.pop_size):
             members[member_indices[i]].scl_fitness = i
             sum_sf += members[member_indices[i]].scl_fitness
 
-    elif scale_type == 3:
-        member_indices = sorted(range(pop_size), key=lambda i: members[i].raw_fitness, reverse=True)
-        for i in range(pop_size):
+    elif params.scale_type == 3:
+        member_indices = sorted(range(params.pop_size), key=lambda i: members[i].raw_fitness, reverse=True)
+        for i in range(params.pop_size):
             members[member_indices[i]].scl_fitness = i
             sum_sf += members[member_indices[i]].scl_fitness
-    
-    for i in range(pop_size):
-        members[i].pro_fitness = members[i].scl_fitness/sum_sf
+
+    for i in range(params.pop_size):
+        members[i].pro_fitness = members[i].scl_fitness / sum_sf
+
+    return members
+
 
 # -----------------------------------------------
 def evolveGeneration(members):
 
     new_members = []
 
-    for i in range(pop_size):
+    for i in range(params.pop_size):
 
-        p_index1 = selectParent()
+        p_index1 = selectParent(members)
         p_index2 = p_index1
         while p_index2 == p_index1:
-            p_index2 = selectParent()
+            p_index2 = selectParent(members)
 
         p1, p2 = members[p_index1], members[p_index2]
 
-        if random.random() < xover_rate:
+        if random.random() < params.xover_rate:
             c1, c2 = xover(p1, p2)
         else:
             c1, c2 = p1.clone(), p2.clone()
@@ -176,131 +214,101 @@ def evolveGeneration(members):
 
     return new_members
 
-# -----------------------------------------------
-# Load parameters
-# -----------------------------------------------
-try:
-    param_file = sys.argv[1]
-except:
-    print("Error: invalid parameter file")
-    sys.exit(1)
-
-verbose = True if len(sys.argv) > 2 and sys.argv[2] == '1' else False
-    
-printDec('Parameter filename: {}'.format(param_file))
-params = getSettings(param_file)
-
-problem_type = params['Problem Type']
-exp_id = params['Experiment ID']
-random_seed = int(params['Random Number Seed'])
-num_runs = int(params['Number of Runs'])
-pop_size = int(params['Population Size'])
-num_gens = int(params['Generations per Run'])
-sel_type = int(params['Selection Method (1)'])
-scale_type = int(params['Fitness Scaling Type (2)'])
-xover_type = int(params['Crossover Type (3)'])
-xover_rate = float(params['Crossover Rate (4)'])
-mut_type = int(params['Mutation Type (5)'])
-mut_rate = float(params['Mutation Rate (6)'])
-num_genes = int(params['Number of Genes/Points (7)'])
-gene_size = int(params['Size of Genes (18)'])
-
-
-printDec('Problem name: {}'.format(problem_type))
-
-out_file = 'results/{}_summary.csv'.format(exp_id)
-writeFile(out_file, '')
-random.seed(random_seed)
-min_or_max  = 'max' if scale_type in [0, 2] else 'min'
-
-# Rank selection
-if sel_type==4:
-    sel_type = 1
-    if scale_type==0:
-        scale_type = 2
-    elif scale_type==1:
-        scale_type = 3
-
 
 # -----------------------------------------------
-# Run GA
-# -----------------------------------------------
-best_overall, best_overall_r, best_overall_g = None, -1, -1
+def runGA(params, verbose=False):
+    printDec('Problem name: {}'.format(params.problem_type))
 
-stats_overall = []
+    out_file = 'results/{}_summary.csv'.format(params.exp_id)
+    writeFile(out_file, '')
+    random.seed(params.random_seed)
+    min_or_max = 'max' if params.scale_type in [0, 2] else 'min'
 
-for r in range(1, num_runs+1):
+    # -----------------------------------------------
+    # Run GA
+    # -----------------------------------------------
+    best_overall, best_overall_r, best_overall_g = None, -1, -1
 
-    members = [Chromosome() for i in range(pop_size)]
-    
-    best_of_run, best_of_run_g = None, -1
+    stats_overall = []
 
-    stats_all_gen = []
+    for r in range(1, params.num_runs + 1):
 
-    perc = 10
+        members = [Chromosome() for i in range(params.pop_size)]
 
-    for g in range(1, num_gens+1):
+        best_of_run, best_of_run_g = None, -1
 
-        if not verbose: perc = showPercBar(g, num_gens, perc)
+        stats_all_gen = []
 
-        sum_rf = 0
-        sum_rf_2 = 0
+        perc = 10
 
-        best_of_gen = None
+        for g in range(1, params.num_gens + 1):
 
-        for i in range(pop_size):
+            if not verbose: perc = showPercBar(g, params.num_gens, perc)
 
-            members[i].calcFitness()
+            sum_rf = 0
+            sum_rf_2 = 0
 
-            sum_rf += members[i].raw_fitness
-            sum_rf_2 += members[i].raw_fitness ** 2
-                
-            if best_of_gen == None or \
-               (min_or_max == 'max' and best_of_gen.raw_fitness < members[i].raw_fitness) or \
-               (min_or_max == 'min' and best_of_gen.raw_fitness > members[i].raw_fitness):
+            best_of_gen = None
 
-                best_of_gen = members[i].clone()
+            for i in range(params.pop_size):
 
-        scaleFitness()
-        members = evolveGeneration(members)
+                members[i].calcFitness()
 
-        avg_rf = sum_rf/pop_size
-        std_dev_rf = math.sqrt(abs(sum_rf_2-sum_rf**2/pop_size)/(pop_size-1))
+                sum_rf += members[i].raw_fitness
+                sum_rf_2 += members[i].raw_fitness ** 2
 
-        if verbose: print('{}\t{}\t{}\t{}\t{}'.format(r, g, best_of_gen.raw_fitness, avg_rf, std_dev_rf))
+                if best_of_gen == None or \
+                        (min_or_max == 'max' and best_of_gen.raw_fitness < members[i].raw_fitness) or \
+                        (min_or_max == 'min' and best_of_gen.raw_fitness > members[i].raw_fitness):
+                    best_of_gen = members[i].clone()
 
-        stats_all_gen.append([r, g, best_of_gen.raw_fitness, avg_rf, std_dev_rf])
+            members = scaleFitness(members)
+            members = evolveGeneration(members)
 
-        if best_of_gen != None and \
-           (best_of_run == None or \
-           (min_or_max == 'max' and best_of_run.raw_fitness < best_of_gen.raw_fitness) or \
-           (min_or_max == 'min' and best_of_run.raw_fitness > best_of_gen.raw_fitness)):
-            
-            best_of_run, best_of_run_g = best_of_gen.clone(), g
+            avg_rf = sum_rf / params.pop_size
+            std_dev_rf = math.sqrt(abs(sum_rf_2 - sum_rf ** 2 / params.pop_size) / (params.pop_size - 1))
 
-    stats_all_gen.append([])
-    stats_all_gen.append([])
-    
-    writeDataTable(stats_all_gen, out_file, mode='a')
+            if verbose: print('{}\t{}\t{}\t{}\t{}'.format(r, g, best_of_gen.raw_fitness, avg_rf, std_dev_rf))
 
-    printDec('Run: {}, Best gen: {}, Best fitness: {}'.format(r, best_of_run_g, best_of_run.raw_fitness))
-    stats_overall.append([r, best_of_run_g, best_of_run.raw_fitness])
+            stats_all_gen.append([r, g, best_of_gen.raw_fitness, avg_rf, std_dev_rf])
 
-    if best_of_run != None and \
-       (best_overall == None or \
-       (min_or_max == 'max' and best_overall.raw_fitness < best_of_run.raw_fitness) or \
-       (min_or_max == 'min' and best_overall.raw_fitness > best_of_run.raw_fitness)):
+            if best_of_gen != None and \
+                    (best_of_run == None or \
+                     (min_or_max == 'max' and best_of_run.raw_fitness < best_of_gen.raw_fitness) or \
+                     (min_or_max == 'min' and best_of_run.raw_fitness > best_of_gen.raw_fitness)):
+                best_of_run, best_of_run_g = best_of_gen.clone(), g
 
-        best_overall, best_overall_g, best_overall_r = best_of_run.clone(), best_of_run_g, r
+        stats_all_gen.append([])
+        stats_all_gen.append([])
 
-writeDataTable(stats_overall, out_file, mode='a')
-printDec('Best Run: {}, Best gen: {}, Best fitness: {}'.format(best_overall_r, best_overall_g, best_overall.raw_fitness))
+        writeDataTable(stats_all_gen, out_file, mode='a')
 
+        printDec('Run: {}, Best gen: {}, Best fitness: {}'.format(r, best_of_run_g, best_of_run.raw_fitness))
+        stats_overall.append([r, best_of_run_g, best_of_run.raw_fitness])
 
+        if best_of_run != None and \
+                (best_overall == None or \
+                 (min_or_max == 'max' and best_overall.raw_fitness < best_of_run.raw_fitness) or \
+                 (min_or_max == 'min' and best_overall.raw_fitness > best_of_run.raw_fitness)):
+            best_overall, best_overall_g, best_overall_r = best_of_run.clone(), best_of_run_g, r
 
-
+    writeDataTable(stats_overall, out_file, mode='a')
+    printDec(
+        'Best Run: {}, Best gen: {}, Best fitness: {}'.format(best_overall_r, best_overall_g, best_overall.raw_fitness))
 
 
+if __name__ == '__main__':
+    # -----------------------------------------------
+    # Load parameters
+    # -----------------------------------------------
+    try:
+        param_file = sys.argv[1]
+    except:
+        print("Error: invalid parameter file")
+        sys.exit(1)
 
+    verbose = True if len(sys.argv) > 2 and sys.argv[2] == '1' else False
 
-    
+    printDec('Parameter filename: {}'.format(param_file))
+    params = Parameters(getSettings(param_file))
+    runGA(params)
